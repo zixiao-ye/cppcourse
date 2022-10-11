694626e91fab328004aa1bffd1ed8a1965d3dae0
Jonas Jelten <jj@sft.lol>
2022-10-11 23:52:37 +0200

    welcome to CIIP C++ course WS2022

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;; emacs settings for all files in the repo
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Directory-Variables.html
+((nil . ((indent-tabs-mode . nil)
+         (tab-width . 4)))
+ (cmake-mode . ((indent-tabs-mode . nil)))
+ (c++-mode . ((c-file-style . "stroustrup"))))
